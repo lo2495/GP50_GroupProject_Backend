8bccb38af44e17196a397bdc12a7f647747b2b5a
lo2495 <lohiyeung@gmail.com>
2023-11-10 00:26:46 +0800

    Database Updated

--- Database/s350f_groupproject_gp50.sql
+++ Database/s350f_groupproject_gp50.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-11-09 14:13:54
+-- 產生時間： 2023-11-09 17:25:33
 -- 伺服器版本： 10.4.21-MariaDB
 -- PHP 版本： 7.3.31
 
@@ -33,19 +33,50 @@ CREATE TABLE `studentrecords` (
   `StudentEmail` varchar(255) NOT NULL,
   `Gender` char(1) NOT NULL,
   `BirthDate` date DEFAULT NULL,
-  `PhoneNumber` varchar(8) DEFAULT NULL
+  `PhoneNumber` varchar(8) DEFAULT NULL,
+  `Status` varchar(255) NOT NULL,
+  `Major` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- 傾印資料表的資料 `studentrecords`
 --
 
-INSERT INTO `studentrecords` (`StudentID`, `Name`, `StudentEmail`, `Gender`, `BirthDate`, `PhoneNumber`) VALUES
-('12345678', 'John Doe', 's1234567@live.hkmu.edu.hk', 'M', '1998-05-15', '12345678'),
-('87654321', 'Jane Smith', 's8765432@live.hkmu.edu.hk', 'F', '1999-09-20', '87654321'),
-('98765432', 'Alex Johnson', 's98765432@live.hkmu.edu.hk', 'M', '2000-12-03', '98765432'),
-('23456789', 'Emily Davis', 's2345678@live.hkmu.edu.hk', 'F', '1997-03-10', '23456789'),
-('34567890', 'Michael Wilson', 's3456789@live.hkmu.edu.hk', 'M', '1996-08-27', '34567890');
+INSERT INTO `studentrecords` (`StudentID`, `Name`, `StudentEmail`, `Gender`, `BirthDate`, `PhoneNumber`, `Status`, `Major`) VALUES
+('12345678', 'John Doe', 's1234567@live.hkmu.edu.hk', 'M', '1998-05-15', '12345678', 'undergraduate', 'Computer Science'),
+('23456789', 'Emily Davis', 's2345678@live.hkmu.edu.hk', 'F', '1997-03-10', '23456789', 'undergraduate', 'Chinese Studies'),
+('34567890', 'Michael Wilson', 's3456789@live.hkmu.edu.hk', 'M', '1996-08-27', '34567890', 'undergraduate', 'Computer Engineering'),
+('87654321', 'Jane Smith', 's8765432@live.hkmu.edu.hk', 'F', '1999-09-20', '87654321', 'undergraduate', 'Computer Science'),
+('98765432', 'Alex Johnson', 's9876543@live.hkmu.edu.hk', 'M', '2000-12-03', '98765432', 'undergraduate', 'Mathematics');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `teacherrecords`
+--
+
+CREATE TABLE `teacherrecords` (
+  `TeacherID` varchar(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Gender` char(1) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `EmploymentDate` varchar(255) NOT NULL,
+  `PhoneNumber` varchar(8) NOT NULL,
+  `Department` varchar(255) NOT NULL,
+  `Designation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `teacherrecords`
+--
+
+INSERT INTO `teacherrecords` (`TeacherID`, `Name`, `Gender`, `Email`, `EmploymentDate`, `PhoneNumber`, `Department`, `Designation`) VALUES
+('Daniel20230901', 'Daniel Kim', 'M', 'danielkim@example.com', '2023-09-01', '74516789', 'Political Science', 'Professor'),
+('David20230701', 'David Lee', 'M', 'davidlee@example.com', '2023-07-01', '87545678', 'History', 'Professor'),
+('Jeff123', 'Jeff', 'M', 'Jeff@example.com', '2023-09-01', '12312323', 'Computer Science', 'Program Leader'),
+('Jennifer20230801', 'Jennifer Chen', 'F', 'jenniferchen@example.com', '2023-08-01', '87654612', 'Economics', 'Associate Professor'),
+('Jessica20231001', 'Jessica Wong', 'F', 'jessicawong@example.com', '2023-10-01', '76213210', 'Sociology', 'Assistant Professor'),
+('Sarah20230601', 'Sarah Johnson', 'F', 'sarahjohnson@example.com', '2023-06-01', '98765455', 'Biology', 'Assistant Professor');
 
 -- --------------------------------------------------------
 
@@ -71,7 +102,28 @@ INSERT INTO `useraccount` (`LoginID`, `password`, `UserRole`, `Name`) VALUES
 ('87654321', '123123', 'student', 'Jane Smith'),
 ('98765432', '123123', 'student', 'Alex Johnson'),
 ('23456789', '123123', 'student', 'Emily Davis'),
-('34567890', '123123', 'student', 'Michael Wilson');
+('34567890', '123123', 'student', 'Michael Wilson'),
+('Sarah20230601', '123123', 'teacher', 'Sarah Johnson'),
+('David20230701', '123123', 'teacher', 'David Lee'),
+('Jennifer20230801', '123123', 'teacher', 'Jennifer Chen'),
+('Daniel20230901', '123123', 'teacher', 'Daniel Kim'),
+('Jessica20231001', '123123', 'teacher', 'Jessica Wong');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `studentrecords`
+--
+ALTER TABLE `studentrecords`
+  ADD PRIMARY KEY (`StudentID`);
+
+--
+-- 資料表索引 `teacherrecords`
+--
+ALTER TABLE `teacherrecords`
+  ADD PRIMARY KEY (`TeacherID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
