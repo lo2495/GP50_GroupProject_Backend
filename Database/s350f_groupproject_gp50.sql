89ec86f9e4981dfd0bc0bb9f3f69f95d6aff266c
lo2495 <lohiyeung@gmail.com>
2023-11-17 00:39:50 +0800

    database updated

--- Database/s350f_groupproject_gp50.sql
+++ Database/s350f_groupproject_gp50.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- 主機： 127.0.0.1
--- 產生時間： 2023-11-15 03:23:47
--- 伺服器版本： 10.4.21-MariaDB
--- PHP 版本： 7.3.31
+-- 主機： db
+-- 產生時間： 2023 年 11 月 17 日 00:38
+-- 伺服器版本： 10.2.9-MariaDB-10.2.9+maria~jessie
+-- PHP 版本： 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,60 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫: `s350f_groupproject_gp50`
+-- 資料庫： `s350f_groupproject_gp50`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `AttendanceRecords`
+--
+
+CREATE TABLE `AttendanceRecords` (
+  `ClassID` varchar(255) NOT NULL,
+  `StudentID` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Status` varchar(255) DEFAULT NULL,
+  `Remarks` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 傾印資料表的資料 `AttendanceRecords`
+--
+
+INSERT INTO `AttendanceRecords` (`ClassID`, `StudentID`, `Date`, `Status`, `Remarks`) VALUES
+('S350FSoftwareEngineering-2023-11-17-16:00', 12345678, '2023-11-17', NULL, NULL),
+('S350FSoftwareEngineering-2023-11-17-16:00', 87654321, '2023-11-17', NULL, NULL),
+('S320FDataBaseManagement-2023-11-21-11:00', 12345678, '2023-11-21', NULL, NULL),
+('S320FDataBaseManagement-2023-11-21-11:00', 87654321, '2023-11-21', NULL, NULL),
+('S312FJavaApplication-2023-11-20-11:00', 12345678, '2023-11-20', NULL, NULL),
+('S312FJavaApplication-2023-11-20-11:00', 87654321, '2023-11-20', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `ClassSchedule`
+--
+
+CREATE TABLE `ClassSchedule` (
+  `ClassID` varchar(255) NOT NULL,
+  `CourseName` varchar(255) NOT NULL,
+  `ClassType` varchar(255) NOT NULL,
+  `ClassDate` date NOT NULL,
+  `StartTime` varchar(255) NOT NULL,
+  `EndTime` varchar(255) NOT NULL,
+  `Venue` varchar(255) NOT NULL,
+  `InstructorName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 傾印資料表的資料 `ClassSchedule`
+--
+
+INSERT INTO `ClassSchedule` (`ClassID`, `CourseName`, `ClassType`, `ClassDate`, `StartTime`, `EndTime`, `Venue`, `InstructorName`) VALUES
+('S312FJavaApplication-2023-11-20-11:00', 'S312FJavaApplication', 'Lecture', '2023-11-20', '11:00', '13:00', 'JCC_DC309', 'JeffAuYeung'),
+('S320FDataBaseManagement-2023-11-21-11:00', 'S320FDataBaseManagement', 'Lecture', '2023-11-21', '11:00', '13:00', 'JCC_DC309', 'JeffAuYeung'),
+('S350FSoftwareEngineering-2023-11-17-16:00', 'S350FSoftwareEngineering', 'Lecture', '2023-11-17', '16:00', '18:00', 'JCC_DC309', 'JeffAuYeung');
 
 -- --------------------------------------------------------
 
@@ -71,7 +123,7 @@ CREATE TABLE `teacherrecords` (
 --
 
 INSERT INTO `teacherrecords` (`TeacherID`, `Name`, `Gender`, `Email`, `EmploymentDate`, `PhoneNumber`, `Department`, `Designation`) VALUES
-('Jeff20231101', 'Jeff', 'M', 'jeff@example.com', '2023-11-01', '12312312', 'Computer Science', 'Program Leader'),
+('Jeff20231101', 'JeffAuYeung', 'M', 'jeff@example.com', '2023-11-01', '12312312', 'Computer Science', 'Program Leader'),
 ('Jennifer20230801', 'Jennifer Chen', 'F', 'jenniferchen@example.com', '2023-08-01', '87654612', 'Economics', 'Associate Professor'),
 ('Jessica20231001', 'Jessica Wong', 'F', 'jessicawong@example.com', '2023-10-01', '76213210', 'Sociology', 'Assistant Professor'),
 ('Sarah20230601', 'Sarah Johnson', 'F', 'sarahjohnson@example.com', '2023-06-01', '98765455', 'Biology', 'Assistant Professor');
@@ -108,6 +160,18 @@ INSERT INTO `useraccount` (`LoginID`, `password`, `UserRole`, `Name`) VALUES
 --
 
 --
+-- 資料表索引 `AttendanceRecords`
+--
+ALTER TABLE `AttendanceRecords`
+  ADD KEY `ClassID` (`ClassID`) USING BTREE;
+
+--
+-- 資料表索引 `ClassSchedule`
+--
+ALTER TABLE `ClassSchedule`
+  ADD PRIMARY KEY (`ClassID`);
+
+--
 -- 資料表索引 `studentrecords`
 --
 ALTER TABLE `studentrecords`
@@ -118,6 +182,16 @@ ALTER TABLE `studentrecords`
 --
 ALTER TABLE `teacherrecords`
   ADD PRIMARY KEY (`TeacherID`);
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `AttendanceRecords`
+--
+ALTER TABLE `AttendanceRecords`
+  ADD CONSTRAINT `attendancerecords_ibfk_1` FOREIGN KEY (`ClassID`) REFERENCES `ClassSchedule` (`ClassID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
