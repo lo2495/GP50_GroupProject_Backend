9397a188c00309a7edcac445a9f19c42fdd88eb6
hahalty <lautszyin1111@gmail.com>
2023-12-04 18:52:47 +0800

    this push

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CORS(app)
 app.secret_key = 'xyzsdfg'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 's350f_groupproject_gp50'
 
 mysql = MySQL(app)
